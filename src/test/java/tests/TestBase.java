ce9b325d46e8ee1d95c543808883337b3f2054b7
PolinaKoriagina <koriagina.p@gmail.com>
2021-06-21 21:46:57 +0200

    added owner test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -26,6 +26,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+      //  Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
