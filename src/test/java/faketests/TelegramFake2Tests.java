b43b4d07aa99d116235965e01f2b3cb725907a31
PolinaKoriagina <koriagina.p@gmail.com>
2021-06-24 20:03:48 +0200

    added fake tests for telegram

---
+++ src/test/java/faketests/TelegramFake2Tests.java
@@ -0,0 +1,47 @@
+package faketests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("fake")
+public class TelegramFake2Tests {
+    @Test
+    @Disabled
+    void someTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void some5Test() {
+        assertTrue(true);
+    }
+
+}
---
+++ src/test/java/faketests/TelegramFakeTests.java
@@ -0,0 +1,40 @@
+package faketests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("fake")
+public class TelegramFakeTests {
+    @Test
+    void someTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some5Test() {
+        assertTrue(true);
+    }
+
+}
